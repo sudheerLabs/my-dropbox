b3bab477b5c97f3c225d8cc36a4a865624982636
sudheersammeta <sudheer.sammeta@sjsu.edu>
2017-10-08 14:15:43 -0700

    Creating the basic setup for the application

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+import { authentication } from './authentication.reducer';
+//import { registration } from './registration.reducer';
+//import { users } from './users.reducer';
+//import { alert } from './alert.reducer';
+
+const rootReducer = combineReducers({
+  authentication
+});
+
+export default rootReducer;
